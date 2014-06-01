d60bcbbd8fd609955c2fd4306f49c2fbeec1cbd4
Jason Smythe <jason@smythemail.za.net>
2014-06-01 22:08:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello World!\nThis is Jason\'s website')
 })
 
 app.listen(app.get('port'), function() {
